92272e3f4f25630e0ffaba915bab3212d92f44f0
Jtyler139 <jtyler0226@gmail.com>
2025-09-17 10:15:42 -0500

    add start button functionality

--- idle_trash/main.py
+++ idle_trash/main.py
@@ -8,7 +8,7 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((screen_width, screen_height))
-    uielement = UIElement(
+    start_button = UIElement(
         center_position=(screen_width//2, screen_height//4),
         font_size=30,
         bg_rgb=(106, 159, 181),
@@ -44,11 +44,13 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if quit_button.mouse_over:
                     running = False
+                if start_button.mouse_over:
+                    start_game()
 
         screen.fill((106, 159, 181))
 
-        uielement.update(pygame.mouse.get_pos())
-        uielement.draw(screen)
+        start_button.update(pygame.mouse.get_pos())
+        start_button.draw(screen)
         quit_button.update(pygame.mouse.get_pos())
         quit_button.draw(screen)
 
@@ -60,6 +62,10 @@ def main():
         pygame.display.update()
 
     pygame.quit()
+
+def start_game():
+    print("Idle Trash Begins!")
+    
 
 
 if __name__ == "__main__":
